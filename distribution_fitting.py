a5454a2bbd4dbb553d1e6fbaea5a1e11f288b566
jgoumaz <56827471+jgoumaz@users.noreply.github.com>
2023-04-12 12:06:32 +0200

    Release draft
    Working version

--- distribution_fitting.py
+++ distribution_fitting.py
@@ -1,13 +1,15 @@
-import gzip, glob, math, random, pickle, time, subprocess, warnings
-import numpy as np
-# from settings_V0_5 import *
+import  warnings
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
-
+import numpy as np
 from scipy.stats import wasserstein_distance
 from scipy import optimize
-from scipy.stats import norm
+
+
+"""##############################
+          SHIFT/RESCALE
+##############################"""
 
 
 def shift_func(x, a):
@@ -25,7 +27,13 @@ def rescale_func(x, a):
     :param a: [float] rescale factor
     :return: [ndarray] rescaled data
     '''
-    return x/a
+    return x/(a+1e-8) # add 1e-8 to avoid "RuntimeWarning: divide by zero"
+
+
+"""##############################
+     DISTRIBUTION FUNCTIONS      
+##############################"""
+
 
 def exponential_decay(x, a):
     '''
@@ -44,26 +52,21 @@ def power_law(x, a, b):
     :return: [ndarray] power law
     '''
     return a*(x**b)
-def power_law_shifted(x, a, b):
+def normal(x, mu, sigma, A):
     '''
-    Power law function shifted -> a(x+c)**b with condition f(0)=1 -> (x/a+1)**b
+    Normal distribution
     :param x: [ndarray or float] data
-    :param a: [float] parameter to optimize
-    :param b: [float] parameter to optimize
-    :return: [ndarray] shifted power law
+    :param mu: [float] mean to optimize
+    :param sigma: [float] standard deviation to optimize
+    :param A: [float] amplitude to optimize
+    :return: [ndarray] normal distribution
     '''
-    return ((x/a+1)**b)
-def power_lawb(x, a, b,c ,d):
-    '''
-    Power law function
-    :param x: [ndarray or float] data
-    :param a: [float] parameter to optimize
-    :param b: [float] parameter to optimize
-    :return: [ndarray] power law
-    '''
-    return a*(((x+c)/d)**b)
+    return A * np.exp(-0.5*np.square((x-mu)/sigma)) / (sigma*np.sqrt(2*np.pi))
 
 
+"""##############################
+   DISTRIBUTION FITTING CLASS      
+##############################"""
 
 
 class Distribution():
@@ -72,7 +75,7 @@ class Distribution():
     :arg self.dist: distribution function [function]
     :arg self.dist_nargs: number of parameters of the distribution function [int]
     :arg self.apply_pre_shift_and_rescale: boolean specifying if shifting/rescaling should be tested before dist function [bool]
-    :arg self.apply_post_shift_and_rescale: boolean specifying if shifting/rescaling should be tested after dist function [bool]
+    :arg self.apply_post_rescale: boolean specifying if rescaling should be tested after dist function [bool]
     :arg self.func: function to optimize (distribution with shifting/rescaling if applied) [function]
     :arg self.parameters: parameter names of the function to optimize [list of string]
     :arg self.fit_done: boolean specifying if the fitting has already been done [bool]
@@ -83,17 +86,17 @@ class Distribution():
     :arg self.wasserstein_distance: wasserstein distance computed as a score of the fitting [float]
     '''
 
-    def __init__(self, distribution_func, pre_shift_and_rescale=True, post_shift_and_rescale=True):
+    def __init__(self, distribution_func, pre_shift_and_rescale=False, post_rescale=False):
         '''
         Constructor of the class Distribution
         :param distribution_func: distribution function [function]
         :param pre_shift_and_rescale: True if shifting/rescaling should be tested before dist function [bool]
-        :param post_shift_and_rescale: True if shifting/rescaling should be tested after dist function [bool]
+        :param post_rescale: True if rescaling should be tested after dist function [bool]
         '''
         self.dist = distribution_func
         self.dist_nargs = self.dist.__code__.co_argcount - 1
         self.apply_pre_shift_and_rescale = pre_shift_and_rescale
-        self.apply_post_shift_and_rescale = post_shift_and_rescale
+        self.apply_post_rescale = post_rescale
 
         self.parameters = []
         if self.apply_pre_shift_and_rescale:
@@ -102,17 +105,12 @@ class Distribution():
         else:
             self.func1 = lambda x, *args: self.dist(x,*args)
         self.parameters = self.parameters + list(self.dist.__code__.co_varnames[1:])
-        if self.apply_post_shift_and_rescale:
-            self.func = lambda x, post_shift, post_rescaling, *args: rescale_func(shift_func(self.func1(x,*args),post_shift),post_rescaling)
-            self.parameters = ["post_shift","post_rescaling"] + self.parameters
+        if self.apply_post_rescale:
+            self.func = lambda x, post_rescaling, *args: rescale_func(self.func1(x,*args),post_rescaling)
+            self.parameters = ["post_rescaling"] + self.parameters
         else:
             self.func = self.func1
 
-        # if self.apply_shift_and_rescale:
-        #     self.func = lambda x, pre_shift, pre_rescaling, post_shift, post_rescaling, *args:\
-        #         rescale_func(shift_func(self.dist(rescale_func(shift_func(x, pre_shift), pre_rescaling),*args),post_shift),post_rescaling)
-        # else:
-        #     self.func = self.dist
         self.fit_done = False
 
     def fit(self, xdata, ydata, it=5000, p0=None):
@@ -127,10 +125,13 @@ class Distribution():
         # Initializing p0 (initial parameter guesses)
         if p0 is None:
             p0 = []
-            if self.apply_pre_shift_and_rescale: p0 += [0, 1]
-            if self.apply_post_shift_and_rescale: p0 += [0, 1]
+            if self.apply_pre_shift_and_rescale: p0 = [0, 1] + p0
+            if self.apply_post_rescale: p0 = [1] + p0
             p0 += self.dist_nargs * [1]
-        # fitting the data and computing optimal parameters (popt)
+        if len(p0) == self.dist_nargs:
+            if self.apply_pre_shift_and_rescale: p0 = [0, 1] + p0
+            if self.apply_post_rescale: p0 = [1] + p0
+        # Fitting the data and computing optimal parameters (popt)
         xdata = np.array(xdata)
         ydata = np.array(ydata)
         self.fit_it = it
@@ -147,22 +148,31 @@ class Distribution():
         :return: wasserstein distance [float]
         '''
         if self.fit_done:
-            ydata_predicted = self.func(xdata, *self.popt)
+            ydata_predicted = self.apply(xdata)
             self.wasserstein_distance = wasserstein_distance(ydata, ydata_predicted)
         else:
-            warnings.warn("wasserstein_distance can't be computed if the fitting has not been done")
+            warnings.warn("Distribution.wasserstein_distance can't be computed if the fitting has not been done")
             self.wasserstein_distance = float('nan')
         return self.wasserstein_distance
+
+    def apply(self, xdata):
+        '''
+        Apply the fitted function to xdata -> compute self.func(xdata,*popt)
+        :param xdata: xdata
+        :return: ydata_predicted using self.func with self.popt
+        '''
+        if self.fit_done:
+            ydata_predicted = self.func(xdata, *self.popt)
+        else:
+            warnings.warn("Distribution.apply can't be computed if the fitting has not been done")
+            ydata_predicted = None
+        return ydata_predicted
 
     def __str__(self):
         '''
         String representation of the distribution with the optimal parameters
         :return: string representation [str]
         '''
-        # if self.apply_shift_and_rescale:
-        #     parameters = ["pre_shift", "pre_rescaling", "post_shift", "post_rescaling"] + list(self.dist.__code__.co_varnames[1:])
-        # else:
-        #     parameters = list(self.dist.__code__.co_varnames[1:])
         if self.fit_done:
             parameters_formatted = ', '.join([f"{self.parameters[i]}={round(self.popt[i],2)}" for i in range(len(self.parameters))])
         else:
@@ -171,62 +181,34 @@ class Distribution():
         return func_name + "(" + parameters_formatted + ")"
 
 
-def plot_primer_random_scores(scores):
-    # scores = scores / np.mean(scores)
-    values, bin_edges = np.histogram(scores, bins=101)
-    bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
-    values, bin_centers = values[values > 0], bin_centers[values > 0]
-    # plt.hist(scores, bins=101)
-    plt.plot(bin_centers, values, '-')
+"""##############################
+        TEST FUNCTIONS          
+##############################"""
 
 
+def test_normal_fitting():
+    np.random.seed(0)
+    def normal_test(x, F):
+        F = 1  # fixed
+        return F * np.exp(-0.5 * np.square(x)) / (np.sqrt(2 * np.pi))
 
-def exponential_decayb(x, a,b,c,d,e):
-    '''
-    Exponential decay function
-    :param x: [ndarray or float] data
-    :param a: [float] parameter to optimize
-    :return: [ndarray] exponential decay
-    '''
-    return (np.exp(-a*(x-b)/c)-d)/e
+    # test data
+    x = 9 * np.sort(np.random.rand(500)) - 3
+    y = 3 * normal(x, mu=1.6, sigma=2, A=1)
 
+    # To test: play with the parameters: 'pre_shift_and_rescale' & 'post_rescale'
+    Dist = Distribution(normal_test, pre_shift_and_rescale=True, post_rescale=False)
+    print(Dist) # Distribution unfitted
+    score = Dist.fit(x, y)
+    print(score) # wasserstein score
+    print(Dist) # Distribution fitted
 
-def normal(x, mu, sigma, a):
-    '''
-    Normal distribution
-    :param x: [ndarray or float] data
-    :param mu: [float] mean to optimize
-    :param sigma: [float] standard deviation to optimize
-    :return: [ndarray] normal distribution
-    '''
-    return a * np.exp(-0.5*np.square((x-mu)/sigma)) / (sigma*np.sqrt(2*np.pi))
+    # Plotting (blue = target | green = fitted)
+    plt.plot(x, y, "b")
+    plt.plot(x, Dist.apply(x), "g")
+    plt.show()
 
-def normal2(x, a):
-    '''
-    Normal distribution
-    :param x: [ndarray or float] data
-    :param mu: [float] mean to optimize
-    :param sigma: [float] standard deviation to optimize
-    :return: [ndarray] normal distribution
-    '''
-    return a*np.exp(-0.5*np.square(x)) / (np.sqrt(2*np.pi))
 
 if __name__ == '__main__':
-    np.random.seed(0)
+    test_normal_fitting()
 
-    # Dist = Distribution(power_law, pre_shift_and_rescale=False, post_shift_and_rescale=True)
-    # print(Dist)
-    # score = Dist.fit([0,1,2,3,4,5,6,7,8,9,10,11,12], [10,6,3,2,1,0.5,0.2,0.15,0.1,0.08,0.06,0.05,0.04])
-    # print(score)
-    # print(Dist)
-
-    x = 9 * np.sort(np.random.rand(500)) - 3
-    y = normal(x, mu=1.6, sigma=2, a=1)
-    Dist = Distribution(normal2, pre_shift_and_rescale=True, post_shift_and_rescale=False)
-    print(Dist)
-    score = Dist.fit(x, y)
-    print(score)
-    print(Dist)
-
-    plt.plot(x, y)
-    plt.show()
